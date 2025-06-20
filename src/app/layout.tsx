f8e0f841587418fad5efbdb6e283b7a0ffd2d6f5
dakheliyah <dakheliyah.lk@gmail.com>
2025-06-20 11:32:21 +0530

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import LocalStorageInitializer from "@/components/LocalStorageInitializer"; // Import the new component
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
       >
         <LocalStorageInitializer /> {/* Add the new component here */}
         {children}
+        <Analytics/>
         <Toaster />
       </body>
     </html>
